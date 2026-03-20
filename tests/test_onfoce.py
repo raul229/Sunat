17590894f2e9e8b6bae896f3bd2a627540fc215b
raul <raulzambranoleon15@gmail.com>
2026-03-20 18:28:47 -0500

    fix: mejora de pruebas unitarias

--- tests/test_onfoce.py
+++ tests/test_onfoce.py
@@ -1,6 +1,20 @@
+import pytest
 from src.ConsultorOF import ConsultorOF
-def test_validar_bloqueados():
-    c=ConsultorOF()
-    data = c.validar_ruc_bloqueado(20522317285)
+
+
+
+@pytest.fixture(scope="session")
+def consultor():
+    c = ConsultorOF()
+    c.cargar_token()
+    return c
+
+def test_validar_bloqueados(consultor):
+    data = consultor.validar_ruc_bloqueado(20522317285)
+    assert data['estado'] == 'bloqueado'
+
+def test_score(consultor):
+    data = consultor.score_crediticio(20522317285)
+    print(data)
+    assert data['score'] == '851'
     
-    assert data['error'] == 'bloqueado'
