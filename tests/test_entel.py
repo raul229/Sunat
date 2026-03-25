443b29fa81e2c891af5ce771a3c5a39fd6ae4155
raul <raulzambranoleon15@gmail.com>
2026-03-25 18:34:07 -0500

    fix: test de funsion verificar token

--- tests/test_entel.py
+++ tests/test_entel.py
@@ -9,6 +9,13 @@ def consultor():
 
 
 def test_evaluar_ruc(consultor):
-    data = consultor.evaluar_ruc("20608846701")
+    data = consultor.evaluar_ruc("20494217296")
     print(data)
+    
     assert data is not None
+    
+def test_verificar_token(consultor):
+
+    print(consultor.verificar_token())
+    
+    assert consultor.verificar_token()
